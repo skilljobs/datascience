00757c1cfb2db0f8bf1cce4d0d57158e2bf0dab5
FarmerAbdulAlim <farmerabdulalim@gmail.com>
2024-12-08 12:23:42 +0600

    banner updated

--- app/components/MainBanner.jsx
+++ app/components/MainBanner.jsx
@@ -1,15 +1,9 @@
+import Image from "next/image";
+
 export default function MainBanner() {
   return (
-    <div
-      style={{
-        backgroundSize: "100% 100vh",
-      }}
-      className='bg-[url("/banner.jpg")] bg-contain min-h-[600px] md:min-h-[800px] relative'
-    >
-      <div
-        className="absolute bottom-0 left-0 right-0 h-32 bg-white"
-        
-      ></div>
+    <div>
+      <Image src="/banner.jpg" alt="main banner" width={1920} height={1080} />
     </div>
-  );
+  )
 }
